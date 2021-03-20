485eadfffd2424dd9420d40427770e42d0d299dc
Игорь Петров <ingvarpetrova@gmail.com>
2021-03-20 15:58:42 +0300

    toDoList Done

---
+++ ToDoList/Models/TodoModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ToDoList.Models
+{
+    class TodoModel : INotifyPropertyChanged
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private bool _isDone;
+        private string _text;
+
+        public bool IsDone
+        {
+            get
+            {
+                return _isDone;
+            }
+            set
+            {
+                if (_isDone == value)
+                {
+                    return;
+                }
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (Text == value)
+                {
+                    return;
+                }
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
